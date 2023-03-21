d24dea20e35e6555fa474c70e14324dbb63c0842
魏全对 <weilindong02@gmail.com>
2023-03-21 14:11:43 +0800

    14:09 21/03/2023

---
+++ ui/ui/ui_ImageViewerUI.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'ImageViewerUITxqLSS.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+from ui.ui.Image_widget import ImageWidget
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(400, 300)
+        self.verticalLayout = QVBoxLayout(Form)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.widget = ImageWidget(Form)
+        self.widget.setObjectName(u"widget")
+
+        self.verticalLayout.addWidget(self.widget)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalSlider = QSlider(Form)
+        self.horizontalSlider.setObjectName(u"horizontalSlider")
+        self.horizontalSlider.setOrientation(Qt.Horizontal)
+
+        self.horizontalLayout.addWidget(self.horizontalSlider)
+
+        self.spinBox = QSpinBox(Form)
+        self.spinBox.setObjectName(u"spinBox")
+
+        self.horizontalLayout.addWidget(self.spinBox)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.verticalLayout.setStretch(0, 9)
+        self.verticalLayout.setStretch(1, 1)
+
+        self.retranslateUi(Form)
+
+        QMetaObject.connectSlotsByName(Form)
+    # setupUi
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"showImage", None))
+    # retranslateUi
+
